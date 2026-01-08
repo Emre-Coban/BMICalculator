227438dc3ad1c163987b9051107b8701a5e501d2
Emre Çoban <emrecoban_1913@hotmail.com>
2026-01-09 00:20:05 +0300

    Added keyboard dismissal logic with FocusState

--- BMICalculator/Views/ContentView.swift
+++ BMICalculator/Views/ContentView.swift
@@ -11,19 +11,53 @@ struct ContentView: View {
     
     @StateObject private var viewModel = BMIViewModel()
     
+    @FocusState private var isInputFocused: Bool
+    
     var body: some View {
         NavigationStack {
-            VStack {
+            VStack(spacing: 20) {
                 TextField("Kilonuz (kg)", value: $viewModel.weight, format: .number)
                     .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad) // Sadece sayı klavyesi açar
+                    .focused($isInputFocused)
                     .padding()
                 
                 TextField("Boyunuz (m)", value: $viewModel.height, format: .number)
                     .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad)
+                    .focused($isInputFocused)
                     .padding()
+                
+                Button("Calculate") {
+                    
+                    viewModel.calculatedBMI()
+                    
+                }
+                .buttonStyle(.borderedProminent) // Butonu mavi ve dolgulu yapar
+                
+                Text("Result: \(String(format: "%.2f", viewModel.bmi))")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .opacity(viewModel.bmi > 0 ? 1 : 0) // Eğer sonuç 0 ise yazı gizlenir
             }
             .navigationTitle("BMI Calculator")
             .navigationBarTitleDisplayMode(.inline)
+            // Klavyenin üzerine "Done" butonu ekler
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer() // butonu en sağa iter
+                    Button("Done") {
+                        isInputFocused = false
+                    }
+                }
+            }
+            // Bu kod boş alanlarında "tıklanabilir" olmasını sağlar.
+            .contentShape(Rectangle())
+            
+            // Boşluğa tıklayınca klavyeyi kapatır
+            .onTapGesture {
+                isInputFocused = false
+            }
         }
     }
 }
