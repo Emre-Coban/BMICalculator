8026e65ec2201a5db240155d5c8d8e634e7ac62d
Emre Çoban <emrecoban_1913@hotmail.com>
2026-01-08 21:15:44 +0300

    Initial Commit

---
+++ BMICalculator/BMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorApp.swift
+//  BMICalculator
+//
+//  Created by EMRE ÇOBAN on 8.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
