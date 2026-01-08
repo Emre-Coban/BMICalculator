ae2ee5dac806fa6367148e00aaa4818f0d82f8f2
Emre Çoban <emrecoban_1913@hotmail.com>
2026-01-08 23:03:54 +0300

    Created BMIModel struct

---
+++ BMICalculator/Models/BMIModel.swift
@@ -0,0 +1,13 @@
+//
+//  BMIModel.swift
+//  BMICalculator
+//
+//  Created by EMRE ÇOBAN on 8.01.2026.
+//
+
+import Foundation
+
+struct BMIModel {
+    var height: Double
+    var weight: Double
+}
